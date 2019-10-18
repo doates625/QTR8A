6197c8b705bfdabe0f715b0d1406c210be67afc7
Dan <doates625@gmail.com>
2019-10-18 12:56:54 -0700

    Moved description to README

--- QTR8A.h
+++ QTR8A.h
@@ -2,20 +2,6 @@
  * @file QTR8A.h
  * @brief Class for interfacing with Pololu QTR-8A analog line sensor
  * @author Dan Oates (WPI Class of 2020)
- * 
- * The QTR-8A is a linear array of 8 analog phototransistors designed for
- * robotic line-following applications. This class contains methods for raw
- * input reading, average calculation, line-detection via thresholding, and
- * line position calculation through center-of-mass. This class has support
- * for both the Arduino and Mbed platforms (see Platform).
- * 
- * Dependencies:
- * - AnalogIn: https://github.com/doates625/AnalogIn.git
- * - Platform: https://github.com/doates625/Platform.git
- * - CppUtil: https://github.com/doates625/CppUtil.git
- * 
- * References:
- * - QTR-8A: https://www.pololu.com/product/960
  */
 #pragma once
 #include <AnalogIn.h>
---
+++ README.md
@@ -0,0 +1,14 @@
+# QTR8A
+Class for interfacing with Pololu QTR-8A analog line sensor  
+Written by Dan Oates (WPI Class of 2020)
+
+### Description
+The QTR-8A is a linear array of 8 analog phototransistors designed for robotic line-following applications. This class contains methods for raw input reading, average calculation, line-detection via thresholding, and line position calculation through center-of-mass. This class has support for both the Arduino and Mbed platforms.
+
+### Dependencies
+- [CppUtil](https://github.com/doates625/CppUtil.git)
+- [Platform](https://github.com/doates625/Platform.git)
+- [AnalogIn](https://github.com/doates625/AnalogIn.git)
+
+### References
+- [Pololu QTR-8A](https://www.pololu.com/product/960)
